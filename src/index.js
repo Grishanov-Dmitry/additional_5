67131046d06f1ef0b7deb41d4aea2f43b15db512
Grishanov_Dmitry <grishanov.ds@gmail.com>
2017-09-25 12:12:03 +0300

    All test are right

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    module.exports = function check(str, bracketsConfig) {
+        // your solution
+        const pairs = bracketsConfig.reduce((acc, [opening, closing]) => Object.assign(acc, { [opening]: closing }), {});
+        const stack = [];
+
+        for(let bracket of str.split('')) {
+            if(bracket === pairs[bracket] && stack[stack.length - 1] === bracket) {
+                stack.pop();
+            } else if(pairs[bracket]){
+                stack.push(bracket);
+            } else if(bracket !== pairs[stack.pop()]) {
+                return false;
+            }
+        }
+
+        return !stack.length;
+    };
+
